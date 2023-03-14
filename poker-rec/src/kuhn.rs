0c883b8158724be821836c2bbd1d1364a93a814e
Nathanael-lscx <95610494+Nathanael-lscx@users.noreply.github.com>
2023-03-14 17:04:41 +0100

    Tweaks to code and implementation of evaluation

--- poker-rec/src/kuhn.rs
+++ poker-rec/src/kuhn.rs
@@ -11,7 +11,7 @@ pub enum ActionSpace {
     CHECK,
     CALL,
     FOLD,
-    NONE,
+    // NONE,
     END,
 }
 impl Eq for ActionSpace {}
@@ -20,24 +20,21 @@ impl Eq for ActionSpace {}
 pub struct Node {
     // pub parent: Option<Box<Node>>,
     is_chance: bool,
-    to_move: Option<u8>,
+    to_move: u8,
     pub cards: Deck,
 
     pub actions_history: Vec<ActionSpace>,
-    actions: Vec<ActionSpace>,
+    pub actions: Vec<ActionSpace>,
     children: HashMap<ActionSpace, Node>,
 }
 
 impl Node {
-    pub fn new(is_chance: bool, to_move: Option<u8>, actions: Vec<ActionSpace>, actions_history: Vec<ActionSpace>, cards: Deck) -> Self {
+    pub fn new(is_chance: bool, to_move: u8, actions: Vec<ActionSpace>, actions_history: Vec<ActionSpace>, cards: Deck) -> Self {
         let mut children = HashMap::new();
-        let mover: Option<u8>;
+        let mover: u8;
 
-        if to_move.is_some() {
-            mover = Some((to_move.unwrap()+1) % 2);
-        } else {
-            mover = Some(0u8);
-        }
+        mover = (to_move+1) % 2;
+        
         
         fn nextactions(a: ActionSpace, actions_hist: &Vec<ActionSpace>) -> Vec<ActionSpace> {
             if (actions_hist.len() == 0) && a == ActionSpace::BET {
@@ -46,11 +43,9 @@ impl Node {
                 vec![ActionSpace::BET]
             } else if (actions_hist.last().unwrap() == &ActionSpace::CHECK) && a == ActionSpace::BET {
                 vec![ActionSpace::FOLD, ActionSpace::CALL]
-            } else if (a==ActionSpace::CALL) || (a==ActionSpace::FOLD) || (actions_hist.last().unwrap() == &ActionSpace::CHECK && a==ActionSpace::CHECK) {
-                vec![ActionSpace::NONE]
             } else {
-                vec![ActionSpace::END]
-            }
+                Vec::new()
+            } 
         }
         
         
@@ -58,18 +53,14 @@ impl Node {
             let mut history = actions_history.clone();
             let next_actions = nextactions(a, &history);
             history.push(a);
-            if next_actions != vec![ActionSpace::END] {
-                children.insert(a, Node::new(
-                    // &self,
-                    is_chance,
-                    mover,
-                    next_actions,
-                    history,
-                    cards.clone(),
-                ));
-            } else {
-                println!("{:?}", next_actions);
-            }
+            children.insert(a, Node::new(
+                // &self,
+                is_chance,
+                mover,
+                next_actions,
+                history,
+                cards.clone(),
+            ));
         }
         
         Node {
@@ -81,11 +72,36 @@ impl Node {
             actions: actions,
             children: children,
         }
-
     }
-
-    // pub fn evaluation()
-
+        
+    pub fn evaluation(&self) -> Vec<i16> {
+        if self.actions.is_empty() {
+            if self.actions_history.last().unwrap()==&ActionSpace::FOLD {
+                if self.to_move == 0 {
+                    vec![1, -1]
+                } else {
+                    vec![-1, 1]
+                }
+            } else if self.actions_history.last().unwrap()==&ActionSpace::CHECK {
+                if self.cards.d[0].val > self.cards.d[1].val {
+                    vec![1, -1]
+                } else {
+                    vec![1, -1]
+                }
+            } else if self.actions_history.last().unwrap()==&ActionSpace::CALL {
+                if self.cards.d[0].val > self.cards.d[1].val {
+                    vec![2, -2]
+                } else {
+                    vec![2, -2]
+                }
+            } else {
+                Vec::new()
+            }
+        } else {
+            Vec::new()
+        }
+    }
 }
+        
 
 
--- poker-rec/src/main.rs
+++ poker-rec/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let deck = Deck::new();
     println!("{:?}", deck);
 
-    let game = Node::new(false, None, vec![ActionSpace::BET, ActionSpace::CHECK], Vec::new(), deck);
+    let game = Node::new(false, 0, vec![ActionSpace::BET, ActionSpace::CHECK], Vec::new(), deck);
     println!("{:?}", game);
 
     // is_chance: bool, to_move: Option<u8>, actions: Vec<ActionSpace>, actions_history: Vec<ActionSpace>, cards: Vec<Card>
