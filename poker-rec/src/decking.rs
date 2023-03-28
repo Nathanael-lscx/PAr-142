b805ac7ef187b933d5648bd25afebf7c2d502ccd
Nathanael-lscx <95610494+Nathanael-lscx@users.noreply.github.com>
2023-03-28 15:43:42 +0200

    Adding root node to iterate through the whole tree

--- poker-rec/src/decking.rs
+++ poker-rec/src/decking.rs
@@ -1,60 +1,102 @@
-/*     
+/*
     Cette bibliothèque crée la structure du jeu de carte (Deck), elle implémente des fonctions
     permettant d'afficher un jeu et de créer des mains aléatoires.
-*/    
-use rand::{thread_rng, seq::SliceRandom};
-
-
-// Création de la classe du jeu
-
+*/
+use rand::{seq::SliceRandom, thread_rng};
+// use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
 // Création de la structure du jeu de carte
-#[derive(Clone, Debug)]
-pub struct Deck{
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Deck {
     pub d: Vec<Card>,
 }
 
 // Implémentation du jeu de carte dans la structure
-impl Deck{
+impl Deck {
     // Lors de la création de la structure on crée un jeu de cartes mélangé
-    pub fn new() -> Deck{
+    pub fn new() -> Deck {
         // La liste vide
-        let mut cardset:Vec<Card> = Vec::new();
+        let mut cardset: Vec<Card> = Vec::new();
 
         // On itère dans les numéros puis dans les couleurs
-        for v in 1..4{
-            cardset.push(Card{
-                val: v,
-            })
+        for v in 1..4 {
+            cardset.push(Card { val: v })
         }
-    
+
         // On mélange
         cardset.shuffle(&mut thread_rng());
 
         // On renvoie la structure
-        Deck{
-            d: cardset,
-        }
+        Deck { d: cardset }
     }
 
     // Pour sortir une carte, on sort la première
-    pub fn get_card(&mut self) -> Card{
+    pub fn get_card(&mut self) -> Card {
         self.d.pop().unwrap()
+    }
+
+    pub fn hardcode(&mut self, conf: &Decks) -> Deck {
+        let mut cardset: Vec<Card> = Vec::new();
+
+        match conf {
+            &Decks::QK => {
+                cardset.push(Card { val: 1 });
+                cardset.push(Card { val: 3 });
+                cardset.push(Card { val: 2 });
+            }
+            &Decks::JK => {
+                cardset.push(Card { val: 2 });
+                cardset.push(Card { val: 3 });
+                cardset.push(Card { val: 1 });
+            }
+            &Decks::JQ => {
+                cardset.push(Card { val: 3 });
+                cardset.push(Card { val: 2 });
+                cardset.push(Card { val: 1 });
+            }
+            &Decks::KQ => {
+                cardset.push(Card { val: 1 });
+                cardset.push(Card { val: 2 });
+                cardset.push(Card { val: 3 });
+            }
+            &Decks::KJ => {
+                cardset.push(Card { val: 2 });
+                cardset.push(Card { val: 1 });
+                cardset.push(Card { val: 3 });
+            }
+            &Decks::QJ => {
+                cardset.push(Card { val: 3 });
+                cardset.push(Card { val: 1 });
+                cardset.push(Card { val: 2 });
+            }
+        }
+
+        Deck { d: cardset }
     }
 }
 
-
 // Structure de la carte
-#[derive(Hash, PartialEq, Copy, Clone, Debug)]
-pub struct Card{
+#[derive(Copy, Clone, Debug, PartialEq, Hash)]
+pub struct Card {
     pub val: u8,
 }
 impl Eq for Card {}
 
 // Création de la carte et de ses fonctions d'affichage
-impl Card{
-    pub fn format_card_str(&self) -> String{
+impl Card {
+    pub fn format_card_str(&self) -> String {
         // format!("{{'Value': '{:?}'}}", self.val)
         format!("Val : {:?}", self.val)
     }
+}
+
+#[derive(EnumIter, Hash, PartialEq, Eq)]
+pub enum Decks {
+    QK,
+    JK,
+    JQ,
+    KQ,
+    KJ,
+    QJ,
 }
--- poker-rec/src/kuhn.rs
+++ poker-rec/src/kuhn.rs
@@ -2,9 +2,10 @@
     Fichier implémentant le kuhn poker récursif
 */
 
+use crate::decking::{Card, Deck, Decks};
 use std::collections::HashMap;
-use crate::decking::Deck;
-use crate::decking::Card;
+use strum::IntoEnumIterator;
+// use strum_macros::EnumIter;
 
 #[derive(PartialEq, Hash, Clone, Copy, Debug)]
 pub enum ActionSpace {
@@ -12,7 +13,6 @@ pub enum ActionSpace {
     CHECK,
     CALL,
     FOLD,
-    // NONE,
     END,
 }
 impl Eq for ActionSpace {}
@@ -25,8 +25,6 @@ pub struct InformationSet {
 
 #[derive(Debug)]
 pub struct Node {
-    // pub parent: Option<Box<Node>>,
-    //is_chance: bool,
     pub to_move: u8,
     pub cards: Deck,
 
@@ -36,43 +34,49 @@ pub struct Node {
 }
 
 impl Node {
-    pub fn new(is_chance: bool, to_move: u8, actions: Vec<ActionSpace>, actions_history: Vec<ActionSpace>, cards: Deck) -> Self {
+    pub fn new(
+        to_move: u8,
+        actions: Vec<ActionSpace>,
+        actions_history: Vec<ActionSpace>,
+        cards: Deck,
+    ) -> Self {
         let mut children = HashMap::new();
         let mover: u8;
 
-        mover = (to_move+1) % 2;
-        
-        
+        mover = (to_move + 1) % 2;
+
         fn nextactions(a: ActionSpace, actions_hist: &Vec<ActionSpace>) -> Vec<ActionSpace> {
             if (actions_hist.len() == 0) && a == ActionSpace::BET {
                 vec![ActionSpace::FOLD, ActionSpace::CALL]
             } else if (actions_hist.len() == 0) && a == ActionSpace::CHECK {
                 vec![ActionSpace::CHECK, ActionSpace::BET]
-            } else if (actions_hist.last().unwrap() == &ActionSpace::CHECK) && a == ActionSpace::BET {
+            } else if (actions_hist.last().unwrap() == &ActionSpace::CHECK) && a == ActionSpace::BET
+            {
                 vec![ActionSpace::FOLD, ActionSpace::CALL]
             } else {
                 Vec::new()
-            } 
+            }
         }
-        
-        
+
         for a in actions.clone() {
             let mut history = actions_history.clone();
             let next_actions = nextactions(a, &history);
             history.push(a);
-            children.insert(a, Node::new(
-                // &self,
-                is_chance,
-                mover,
-                next_actions,
-                history,
-                cards.clone(),
-            ));
+            children.insert(
+                a,
+                Node::new(
+                    // &self,
+                    mover,
+                    next_actions,
+                    history,
+                    cards.clone(),
+                ),
+            );
         }
-        
+
         Node {
             // parent: parent,
-            //is_chance: is_chance,
+            // is_chance: is_chance,
             to_move: to_move,
             cards: cards,
             actions_history: actions_history,
@@ -97,22 +101,22 @@ impl Node {
             actions_history: self.actions_history.clone(),
         }
     }
-        
+
     pub fn evaluation(&self) -> Vec<i16> {
         if self.actions.is_empty() {
-            if self.actions_history.last().unwrap()==&ActionSpace::FOLD {
+            if self.actions_history.last().unwrap() == &ActionSpace::FOLD {
                 if self.to_move == 0 {
                     vec![1, -1]
                 } else {
                     vec![-1, 1]
                 }
-            } else if self.actions_history.last().unwrap()==&ActionSpace::CHECK {
+            } else if self.actions_history.last().unwrap() == &ActionSpace::CHECK {
                 if self.cards.d[0].val > self.cards.d[1].val {
                     vec![1, -1]
                 } else {
                     vec![-1, 1]
                 }
-            } else if self.actions_history.last().unwrap()==&ActionSpace::CALL {
+            } else if self.actions_history.last().unwrap() == &ActionSpace::CALL {
                 if self.cards.d[0].val > self.cards.d[1].val {
                     vec![2, -2]
                 } else {
@@ -126,6 +130,23 @@ impl Node {
         }
     }
 }
-        
 
+pub struct Root {
+    children: HashMap<Decks, Node>,
+}
 
+impl Root {
+    pub fn new() -> Root {
+        let mut mychildren = HashMap::new();
+
+        for mydeck in Decks::iter() {
+            let mut mycards = Deck::new();
+            mycards.hardcode(&mydeck);
+            mychildren.insert(mydeck, Node::new(0, Vec::new(), Vec::new(), mycards));
+        }
+
+        Root {
+            children: mychildren,
+        }
+    }
+}
